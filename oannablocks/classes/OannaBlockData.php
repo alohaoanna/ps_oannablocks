0ab0aa28e0427301032ad303f1b0099dd6e813fb
Geoffrey Touette <geoffrey@oanna.be>
2023-10-16 16:07:28 +0200

    Fix Oannablockdata to include lang_id

--- oannablocks/classes/OannaBlockData.php
+++ oannablocks/classes/OannaBlockData.php
@@ -1,27 +1,27 @@
 <?php
 /**
-* 2007-2017 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    Apply Novation <applynovation@gmail.com>
-*  @copyright 2016-2017 Apply Novation
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*/
+ * 2007-2017 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    Apply Novation <applynovation@gmail.com>
+ *  @copyright 2016-2017 Apply Novation
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
 
 class OannaBlockData extends ObjectModel
 {
@@ -49,6 +49,12 @@ class OannaBlockData extends ObjectModel
         return $this;
     }
 
+    public function setIdLang($id_lang)
+    {
+        $this->id_lang = (int)$id_lang;
+        return $this;
+    }
+
     public function setData($data)
     {
         $this->data = array();
@@ -62,7 +68,7 @@ class OannaBlockData extends ObjectModel
     public function export()
     {
         $data = $this->getData();
-        
+
         return array(
             'id_oannablock' => $this->id_oannablock,
             'id_oannablock_data' => $this->id_oannablock_data,
@@ -104,10 +110,10 @@ class OannaBlockData extends ObjectModel
     {
         if ($this->data !== null) {
             $data = $this->data;
-			if (is_array($data)) {
-				$data = $data[Context::getContext()->language->id];
-			}
-			$data = (array)$data;
+            if (is_array($data)) {
+                $data = $data[Context::getContext()->language->id];
+            }
+            $data = (array)$data;
             $this->_data = json_decode(array_shift($data), true);
         }
         return $this;
